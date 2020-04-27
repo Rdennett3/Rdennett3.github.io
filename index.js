f48168a29e615586257d1a9d73e912f62c7cdb18
Bob Dennett <r.dennett3@gmail.com>
2020-04-26 21:11:42 -0400

    added animations to work page - smoother scrollmagic based animations coming

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+// $(document).ready(function(){
+// var controller = new ScrollMagic.Controller();
+//
+// var ourScene = new ScrollMagic.Scene({
+//     triggerElement:'#one'
+// })
+// .setClassToggle('#kwcImg', 'fade-in')
+// .addTo(controller);
+//
+// });
+
+
+	$(window).scroll(function() {
+		$('#one, #two, #three, #four, #five, #six').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideUp");
+			}
+		});
+	});
+
+
 function openNav() {
   document.getElementById("myNav").style.width = "60%";
 }
