4baaa9c9103d2f4ded1675391b2d92a8d5f0e29e
Bob Dennett <r.dennett3@gmail.com>
2020-05-01 15:55:46 -0400

    new work page layout

--- index.js
+++ index.js
@@ -54,3 +54,25 @@ $(function() {
     transition:'zoomOut'
   });
 });
+
+
+// $(function() {
+//   $.scrollify({
+//     section : ".workSection",
+//     sectionName : "section-name",
+//     interstitialSection : "",
+//     easing: "easeOutExpo",
+//     scrollSpeed: 100,
+//     offset : 0,
+//     scrollbars: true,
+//     standardScrollElements: "",
+//     setHeights: true,
+//     overflowScroll: true,
+//     updateHash: true,
+//     touchScroll:true,
+//     before:function() {},
+//     after:function() {},
+//     afterResize:function() {},
+//     afterRender:function() {}
+//   });
+// });
